9f2e026e10f776b041a6e329e859173f2ef11d0f
tomitheninja <suditomi@pm.me>
2019-10-10 14:42:24 +0200

    Rename screen Form1 to Menu

--- src/Menu.Designer.cs
+++ src/Menu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TowerBlocks
 {
-    partial class Form1
+    partial class Menu
     {
         /// <summary>
         /// Required designer variable.
--- src/Menu.cs
+++ src/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TowerBlocks
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
         }
